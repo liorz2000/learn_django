73ebba70e1792bca71f892deaf82ffb880e117d5
liorz2000 <liorz2000@gmail.com>
2022-12-29 12:08:36 +0200

    add links to views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def page1(request):
+    return render(request, 'page1.html')
+
+def page2(request):
+    return render(request, 'page2.html')
