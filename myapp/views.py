bf3f8fe5d493aa0688ad1e53051bc6773f9ce78f
liorz2000 <liorz2000@gmail.com>
2023-01-05 16:55:24 +0200

    fixed the debbuging

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
-print(settings.TEMPLATE_DIRS)
+print(settings.TEMPLATES[0]['DIRS'])
 
 def page1(request):
     return render(request, 'page1.html')
